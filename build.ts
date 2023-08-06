13ec3ec9388e060589225b8f8a4d1d2081ffca8f
Julián Cruciani <julian@dagger.io>
2023-08-05 22:05:00 -0300

    apply latest version to build script
    Signed-off-by: Julián Cruciani <julian@dagger.io>

--- build.ts
+++ build.ts
@@ -1,39 +1,25 @@
-import Client, { connect, Container, Directory } from "@dagger.io/dagger";
+import { connect, Client, Directory } from "@dagger.io/dagger";
 
 function bash(command: string) {
   return ["bash", "-c", command];
 }
 
-function base(client: Client, repo: Directory): Container {
-  return (
-    client
-      .pipeline("frontend-base-image")
-      .container()
-      .from("node:16.19-alpine")
-      .withExec(["apk", "add", "--no-cache", "bash"])
-      .withExec(
-        bash(
-          "apk add --no-cache build-base git cairo-dev pango-dev jpeg-dev giflib-dev librsvg-dev"
-        )
-      )
-      .withWorkdir("/src")
-      .withExec(bash("yarn install --frozen-lockfile"))
-      .withDirectory("/src", repo)
-      .withExec(bash("yarn install --frozen-lockfile"))
-  );
-}
-
-async function build(client: Client, repo: Directory) {
-  const baseContainer = await base(client, repo)
-    .pipeline("frontend-build")
+async function base(client: Client, repo: Directory) {
+  return await client
+    .pipeline("frontend-base-image")
+    .container()
+    .from("node:16.19-alpine")
+    .withExec(["apk", "add", "--no-cache", "bash"])
     .withExec(
       bash(
-        'NODE_OPTIONS="--max_old_space_size=16384" yarn exec -- vite build'
+        "apk add --no-cache build-base git cairo-dev pango-dev jpeg-dev giflib-dev librsvg-dev"
       )
     )
+    .withWorkdir("/src")
+    .withExec(bash("yarn install --frozen-lockfile"))
+    .withDirectory("/src", repo)
+    .withExec(bash("yarn install --frozen-lockfile"))
     .sync();
-
-  return baseContainer;
 }
 
 async function main() {
@@ -41,7 +27,17 @@ async function main() {
     const repo = client.host().directory(".", {
       exclude: [".git", "node_modules", "dist", "build"],
     });
-    await build(client, repo);
+
+    const baseContainer = await base(client, repo);
+
+    baseContainer
+      .pipeline("frontend-build")
+      .withExec(
+        bash(
+          'NODE_OPTIONS="--max_old_space_size=16384" yarn exec -- vite build'
+        )
+      )
+      .sync();
   });
 }
 
